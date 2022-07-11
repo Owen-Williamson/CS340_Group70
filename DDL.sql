1189674e1c6c4572d2dbcc6107842aa69fa002da
Owen-Williamson <willowen@oregonstate.edu>
2022-07-11 15:10:18 -0700

    Fixed all table creation typos

--- DDL.sql
+++ DDL.sql
@@ -19,12 +19,12 @@ CREATE TABLE Facilities (
 
 CREATE TABLE Trucks (
   truck_id int NOT NULL AUTO_INCREMENT,
+  current_facility_id int,
   max_weight int NOT NULL,
   max_volume int NOT NULL,
-  current_facility_id int,
 
   PRIMARY KEY (truck_id),
-  FOREIGN KEY (current_facility_id) REFERENCES Facility(facility_id)
+  FOREIGN KEY (current_facility_id) REFERENCES Facilities(facility_id)
 );
 
 CREATE TABLE Drivers (
@@ -37,20 +37,20 @@ CREATE TABLE Drivers (
   PRIMARY KEY (driver_id)
 );
 
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
   order_id int NOT NULL AUTO_INCREMENT,
-  customer_id int NOT NULL,
-  start_facility_id int NOT NULL,
-  end_facility_id int NOT NULL,
+  customer_id int,
+  start_facility_id int,
+  end_facility_id int,
   current_facility_id int,
   volume int NOT NULL,
   weight int NOT NULL,
 
   PRIMARY KEY (order_id),
   FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-  FOREIGN KEY (start_facility_id) REFERENCES Facilites(facility_id),
-  FOREIGN KEY (end_facility_id) REFERENCES Facilites(facility_id),
-  FOREIGN KEY (current_facility_id) REFERENCES Facilites(facility_id)
+  FOREIGN KEY (start_facility_id) REFERENCES Facilities(facility_id),
+  FOREIGN KEY (end_facility_id) REFERENCES Facilities(facility_id),
+  FOREIGN KEY (current_facility_id) REFERENCES Facilities(facility_id)
 );
 
 CREATE TABLE Deliveries (
@@ -59,7 +59,6 @@ CREATE TABLE Deliveries (
   truck_id int NOT NULL,
   start_facility_id int NOT NULL,
   end_facility_id int NOT NULL,
-
   total_volume int NOT NULL,
   total_weight int NOT NULL,
 
@@ -69,16 +68,15 @@ CREATE TABLE Deliveries (
   actual_arrival_time datetime,
 
   PRIMARY KEY (delivery_id),
-  FOREIGN KEY (driver_id) REFERENCES DRIVERS(driver_id),
-  FOREIGN KEY (truck_id) REFERENCES TRUCKS(truck_id),
-  FOREIGN KEY (start_facility_id) REFERENCES Facilites(facility_id),
-  FOREIGN KEY (end_facility_id) REFERENCES Facilites(facility_id)
+  FOREIGN KEY (driver_id) REFERENCES Drivers(driver_id),
+  FOREIGN KEY (truck_id) REFERENCES Trucks(truck_id),
+  FOREIGN KEY (start_facility_id) REFERENCES Facilities(facility_id),
+  FOREIGN KEY (end_facility_id) REFERENCES Facilities(facility_id)
 );
 
 CREATE TABLE DeliveryOrders (
   delivery_id int NOT NULL,
   order_id int NOT NULL,
-
   FOREIGN KEY (delivery_id) REFERENCES Deliveries(delivery_id),
   FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 );
@@ -86,7 +84,6 @@ CREATE TABLE DeliveryOrders (
 CREATE TABLE TruckDrivers (
   driver_id int NOT NULL,
   truck_id int NOT NULL,
-
   FOREIGN KEY (driver_id) REFERENCES Drivers(driver_id),
   FOREIGN KEY (truck_id) REFERENCES Trucks(truck_id)
 );
