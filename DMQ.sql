e053ff9a1ebfd74fdc078ca94fc9276be3e63bf3
Owen-Williamson <willowen@oregonstate.edu>
2022-07-18 20:38:11 -0700

    Added update functions

--- DMQ.sql
+++ DMQ.sql
@@ -6,11 +6,17 @@ VALUES (:plate, :max_weight, :max_volume, :cf_id);
 
 DELETE FROM Trucks WHERE truck_id=:t_id;
 
+UPDATE Trucks SET plate=:plate, max_volume=:max_volume, max_weight=:max_weight, current_facility_id=:cf_id
+WHERE truck_id=:t_id;
+
 -- Drivers
 INSERT INTO Drivers (name, email, late_deliveries, early_deliveries)
 VALUES (:name, :email, :ld, :ed);
 
 DELETE FROM Drivers WHERE driver_id=:d_id;
+
+UPDATE Drivers SET name=:name, email=:email, late_deliveries=:ld, early_deliveries=:ed
+WHERE driver_id=:d_id;
 
 -- Facilities
 INSERT INTO Facilities (name, location, customer_id)
@@ -18,11 +24,17 @@ VALUES (:name, :location, :cid);
 
 DELETE FROM Facilities WHERE facility_id=:f_id;
 
+UPDATE Facilities SET name=:name, location=:location, customer_id=:c_id
+WHERE facility_id=:f_id;
+
 -- Customers
 INSERT INTO Customers (name, email)
 VALUES (:name, :email);
 
 DELETE FROM Customers WHERE customer_id=:c_id;
+
+UPDATE Customers SET name=:name, email=:email
+WHERE customer_id=:c_id;
 
 -- Deliveries
 INSERT INTO Deliveries (departure_time, expected_arrival_time, actual_arrival_time, total_volume, total_weight,
@@ -31,11 +43,18 @@ VALUES (:dt, :eat, :aat, :total_volume, :total_weight, :t_id, :d_id, :sf_id, :ef
 
 DELETE FROM Deliveries WHERE delivery_id=:d_id;
 
+UPDATE Deliveries SET departure_time=:dt, expected_arrival_time=:eat, actual_arrival_time=:aat, total_volume=:total_volume
+                      total_weight=:total_weight, truck_id=:t_id, driver_id=:d_id, start_facility_id=:sf_id, end_facility_id=:ef_id
+WHERE delivery_id=:d_id;
+
 -- Orders
 INSERT INTO Orders (customer_id, volume, weight, start_facility_id, end_facility_id, current_facility_id)
 VALUES (:c_if, :volume, :weight, :sf_id, :ef_id, :cf_id);
 
 DELETE FROM Orders WHERE order_id=:o_id;
+
+UPDATE Orders SET customer_id=:c_id, volume=:volume, weight=:weight, start_facility_id=:sf_id, end_facility_id=:ef_id, current_facility_id=:cf_id
+WHERE order_id=:o_id;
 
 -- TruckDrivers
 INSERT INTO TruckDrivers (truck_id, driver_id)
@@ -44,9 +63,21 @@ VALUES (:t_rd, :d_id);
 DELETE FROM TruckDrivers WHERE driver_id=:d_id;
 DELETE FROM TruckDrivers WHERE truck_id=:t_id;
 
+UPDATE TruckDrivers SET driver_id=:d_id
+WHERE  truck_id=:t_id;
+
+UPDATE TruckDrivers SET truck_id=:t_id
+WHERE  driver_id=:d_id;
+
 -- DeliveryOrders
 INSERT INTO DeliveryOrders (delivery_id, order_id)
 VALUES (:d_id, :o_id);
 
 DELETE FROM DeliveryOrders WHERE delivery_id=:d_id;
 DELETE FROM DeliveryOrders WHERE order_id=:o_id;
+
+UPDATE DeliveryOrders SET delivery_id=:d_id
+WHERE  order_id=:o_id;
+
+UPDATE DeliveryOrders SET order_id=:o_id
+WHERE  delivery_id=:d_id;
