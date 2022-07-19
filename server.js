ca36a83b932c6c13e663dfa9c67abe67a66d0273
Owen-Williamson <willowen@oregonstate.edu>
2022-07-18 17:16:49 -0700

    setup server.js for all 6 entities

--- server.js
+++ server.js
@@ -48,6 +48,51 @@ app.get('/drivers', function(req, res) {
   );
 });
 
+app.get('/trucks', function(req, res) {
+  res.status(200).render("trucks",{
+    trucks: [
+      {"id":"0", "current_facility_id":"0", "plate":"0", "max_weight":"0", "min_weight":"0"},
+      {"id":"1", "current_facility_id":"1", "plate":"1", "max_weight":"1", "min_weight":"1"}
+    ]}
+  );
+});
+
+app.get('/customers', function(req, res) {
+  res.status(200).render("customers",{
+    customers: [
+      {"id":"0", "name":"0", "email":"0"},
+      {"id":"1", "name":"1", "email":"1"}
+    ]}
+  );
+});
+
+app.get('/facilities', function(req, res) {
+  res.status(200).render("facilities",{
+    facilities: [
+      {"id":"0", "customer_id":"0", "location":"0", "name":"0"},
+      {"id":"1", "customer_id":"1", "location":"1", "name":"1"}
+    ]}
+  );
+});
+
+app.get('/deliveries', function(req, res) {
+  res.status(200).render("deliveries",{
+    deliveries: [
+      {"id":"0", "driver_id":"0", "truck_id":"0", "start_facility_id":"0", "end_facility_id":"0", "total_volume":"0", "total_weight":"0", "late":"0", "departure_time":"0","expected_arrival_time":"0", "actual_arrival_time":"0"},
+      {"id":"0", "driver_id":"0", "truck_id":"0", "start_facility_id":"0", "end_facility_id":"0", "total_volume":"0", "total_weight":"0", "late":"0", "departure_time":"0","expected_arrival_time":"0", "actual_arrival_time":"0"}
+    ]}
+  );
+});
+
+app.get('/orders', function(req, res) {
+  res.status(200).render("orders",{
+    orders: [
+      {"id":"0", "customer_id":"0", "start_facility_id":"0", "end_facility_id":"0", "current_facility_id":"0", "volume":"0", "weight":"0"},
+      {"id":"1", "customer_id":"1", "start_facility_id":"1", "end_facility_id":"1", "current_facility_id":"1", "volume":"1", "weight":"1"}
+    ]}
+  );
+});
+
 // add the listener
 app.listen(PORT, function(){
   console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
