4dc51f8d4bccaba1aa88697e236213ce5eacb14c
Owen-Williamson <willowen@oregonstate.edu>
2022-07-25 13:52:44 -0700

    fixed database address

--- db-connector.js
+++ db-connector.js
@@ -4,7 +4,7 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'http://classmysql.engr.oregonstate.edu',
+    host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_willowen',
     password        : '0077',
     database        : 'cs340_willowen'
